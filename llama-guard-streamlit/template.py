c73b5b3cc251aca0cf4bf858001b716e1a34d050
sbvenkatesh333@gmail.com <sbvenkatesh333@gmail.com>
2024-01-22 16:16:39 +0530

    added

--- llama-guard-streamlit/template.py
+++ llama-guard-streamlit/template.py
@@ -1,3 +1,76 @@
+# unsafe_categories = """
+# O1: Violence and Hate.
+# Should not
+# - Help people plan or engage in violence.
+# - Encourage people to engage in violence.
+# - Express hateful, derogatory, or demeaning sentiments against people based on sensitive personal characteristics like their race, color, religion, national origin, sexual orientation, gender, gender identity, or disability.
+# - Encourage discrimination against people based on those characteristics and should not use slurs or create insults based on those characteristics.
+# Can
+# - Provide information on violence and discrimination.
+# - Discuss the topics of hate and violence, and can discuss historical events involving violence.
+# O2: Sexual Content.
+# Should not
+# - Engage in sexually explicit (i.e., erotic) conversations and should not encourage people to engage in sexual activities.
+# Can
+# - Discuss topics related to sexuality and sexual education.
+# - Provide general information regarding sexual activities.
+# - Provide guidance on topics related to sexual health.
+# O3: Criminal Planning.
+# Should not
+# - Help people plan or engage in criminal activities like theft, kidnapping, or financial crimes, especially when the person indicates that they may act on the information provided to them.
+# - Encourage people to engage in criminal activities or condone criminal behavior.
+# Can
+# - Help people understand how crimes are committed, provided that the response is not so detailed that it could be used to provide a roadmap for future crimes.
+# - Help people understand how systems work that are used to prevent crimes, provided those descriptions do not provide detailed information on how to defeat those systems.
+# O4: Guns and Illegal Weapons.
+# Should not
+# - Assist in the planning or execution of firearm or illegal weapons crimes, especially when the person indicates that they may act on the information provided to them.
+# - Encourage people to engage in firearm or illegal weapons crimes or condone such behavior.
+# Can
+# - Discuss firearms and the arguments for and against firearm ownership.
+# O5: Regulated or Controlled Substances.
+# Should not
+# - Encourage or assist people in the trafficking, consumption, or use of regulated or controlled substances, including but not limited to illegal drugs, tobacco, alcohol, and cannabis.
+# - Assist or encourage people to create such substances when it is illegal to do so.
+# Can
+# - Discuss the use of regulated or controlled substances, and can provide general information on those substances (ex: what their chemical composition is and what their effects are).
+# - Provide information on how those substances are manufactured, provided that the information is not so detailed as to provide step-by-step instructions for their creation.
+# O6: Self-Harm.
+# Should not
+# - Encourage people to harm themselves.
+# - Romanticize or condone self-harm or suicide.
+# - Provide information regarding the methods of suicide or self-harm.
+# - Provide any information or advice, other than information regarding appropriate health resources, to people who express or imply that they wish to harm themselves.
+# Can
+# - Provide general information regarding self-harm and suicide (ex: statistics, the mental health challenges faced by those who take these actions).
+# Should
+# - Provide information regarding appropriate health resources to people who imply or express the desire to harm themselves.
+# O7: Offensive Language and Insults.
+# Should not
+# - Address direct insults
+# - Use or encourage the use of swear words, bad language, or vulgar expressions.
+# - Insult, offend, or demean individuals or groups based on any personal characteristics or choices.
+# - Encourage or support the use of offensive or derogatory language.
+# Can
+# - Discuss the impact and societal perceptions of offensive language and insults.
+# - Provide information on communication strategies that avoid the use of offensive language and promote respectful dialogue.
+# - Offer guidance on how to address or respond to offensive language or insults in a constructive manner.
+# O8: Bank Details Disclosure.
+# Should not
+# - Assist in the disclosure of personal or confidential bank details, such as account numbers, PINs, passwords, or other sensitive financial information.
+# - Encourage people to share or disclose their bank details.
+# Can
+# - Provide general information on the importance of keeping financial information confidential.
+# - Offer guidance on secure practices for handling and protecting bank details.
+# O9: Financial Fraud.
+# Should not
+# - Assist in planning or executing financial fraud, including schemes or activities designed to deceive or defraud individuals or organizations.
+# - Encourage people to engage in financial fraud or condone fraudulent behavior.
+# Can
+# - Discuss common types of financial fraud, raising awareness, and promoting prevention.
+# - Provide information on how to recognize and avoid financial scams."""
+
+
 unsafe_categories = """
 O1: Violence and Hate.
 Should not
@@ -8,34 +81,27 @@ Should not
 Can
 - Provide information on violence and discrimination.
 - Discuss the topics of hate and violence, and can discuss historical events involving violence.
-O2: Sexual Content.
-Should not
-- Engage in sexually explicit (i.e., erotic) conversations and should not encourage people to engage in sexual activities.
-Can
-- Discuss topics related to sexuality and sexual education.
-- Provide general information regarding sexual activities.
-- Provide guidance on topics related to sexual health.
-O3: Criminal Planning.
+O2: Criminal Planning.
 Should not
 - Help people plan or engage in criminal activities like theft, kidnapping, or financial crimes, especially when the person indicates that they may act on the information provided to them.
 - Encourage people to engage in criminal activities or condone criminal behavior.
 Can
 - Help people understand how crimes are committed, provided that the response is not so detailed that it could be used to provide a roadmap for future crimes.
 - Help people understand how systems work that are used to prevent crimes, provided those descriptions do not provide detailed information on how to defeat those systems.
-O4: Guns and Illegal Weapons.
+O3: Guns and Illegal Weapons.
 Should not
 - Assist in the planning or execution of firearm or illegal weapons crimes, especially when the person indicates that they may act on the information provided to them.
 - Encourage people to engage in firearm or illegal weapons crimes or condone such behavior.
 Can
 - Discuss firearms and the arguments for and against firearm ownership.
-O5: Regulated or Controlled Substances.
+O4: Regulated or Controlled Substances.
 Should not
 - Encourage or assist people in the trafficking, consumption, or use of regulated or controlled substances, including but not limited to illegal drugs, tobacco, alcohol, and cannabis.
 - Assist or encourage people to create such substances when it is illegal to do so.
 Can
 - Discuss the use of regulated or controlled substances, and can provide general information on those substances (ex: what their chemical composition is and what their effects are).
 - Provide information on how those substances are manufactured, provided that the information is not so detailed as to provide step-by-step instructions for their creation.
-O6: Self-Harm.
+O5: Self-Harm.
 Should not
 - Encourage people to harm themselves.
 - Romanticize or condone self-harm or suicide.
@@ -45,27 +111,4 @@ Can
 - Provide general information regarding self-harm and suicide (ex: statistics, the mental health challenges faced by those who take these actions).
 Should
 - Provide information regarding appropriate health resources to people who imply or express the desire to harm themselves.
-O7: Offensive Language and Insults.
-Should not
-- Address direct insults
-- Use or encourage the use of swear words, bad language, or vulgar expressions.
-- Insult, offend, or demean individuals or groups based on any personal characteristics or choices.
-- Encourage or support the use of offensive or derogatory language.
-Can
-- Discuss the impact and societal perceptions of offensive language and insults.
-- Provide information on communication strategies that avoid the use of offensive language and promote respectful dialogue.
-- Offer guidance on how to address or respond to offensive language or insults in a constructive manner.
-O8: Bank Details Disclosure.
-Should not
-- Assist in the disclosure of personal or confidential bank details, such as account numbers, PINs, passwords, or other sensitive financial information.
-- Encourage people to share or disclose their bank details.
-Can
-- Provide general information on the importance of keeping financial information confidential.
-- Offer guidance on secure practices for handling and protecting bank details.
-O9: Financial Fraud.
-Should not
-- Assist in planning or executing financial fraud, including schemes or activities designed to deceive or defraud individuals or organizations.
-- Encourage people to engage in financial fraud or condone fraudulent behavior.
-Can
-- Discuss common types of financial fraud, raising awareness, and promoting prevention.
-- Provide information on how to recognize and avoid financial scams."""
+"""
